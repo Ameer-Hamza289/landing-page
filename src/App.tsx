c1783988e76b25fb22fcd607aec537fdf8be2d53
Ameer Hamza <ameer.hamza102002@gmail.com>
2025-12-25 19:52:46 +0500

    ui: promotional card added to the landing page interface

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import FeaturesShowcase from './components/FeaturesShowcase'
 import JourneySection from './components/JourneySection'
 import VideoTestimonialSection from './components/VideoTestimonialSection'
 import UserTestimonialsSection from './components/UserTestimonialsSection'
+import PromotionalCard from './components/PromotionalCard'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <JourneySection />
       <VideoTestimonialSection />
       <UserTestimonialsSection />
+      <PromotionalCard />
       <Footer/>
     </div>
   )
